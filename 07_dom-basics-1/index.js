6f18079c51b7d42fabdce1efeae840ebd7567d67
maximkrinitsyn88 <78745930+maximkrinitsyn88@users.noreply.github.com>
2021-06-03 21:28:35 +0300

    07_changes
    07_changes

--- 07_dom-basics-1/index.js
+++ 07_dom-basics-1/index.js
@@ -1,18 +1,66 @@
+// TASK 1
+
 document.addEventListener('DOMContentLoaded', function() {
   let inputSeconds = document.querySelector('.input-seconds');
   let startTimer = document.querySelector('.start-timer');
   let timer = document.querySelector('.timer');
 
   startTimer.addEventListener('click', function() {
-    timer.innerHTML = inputSeconds.value
+    timer.innerHTML = inputSeconds.value;
     let interval = setInterval(function() {
       if (timer.innerHTML <= 1) {
-        clearInterval(interval)
+        clearInterval(interval);
       }
-      timer.innerHTML--
+      timer.innerHTML--;
     }, 1000)
   });
+
+
+
+// TASK 2
+
+
+let inputHeadline = document.createElement('input');
+document.body.append(inputHeadline);
+let headline = document.createElement('h2');
+document.body.append(headline);
+headline.innerHTML = '';
+
+let enterHeadline = inputHeadline.addEventListener('input', delayHeadline)
+
+function delayHeadline() {
+  let timeout = setTimeout(function() {
+    headline.innerHTML = inputHeadline.value;
+  }, 300);
+  if (enterHeadline) {
+    clearTimeout(timeout)
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
 
 
 
